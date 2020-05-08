6eafe51e759ed16a97be56625b6d1af1d48193df
ajaypadwal73 <51758104+ajaypadwal73@users.noreply.github.com>
2020-05-09 00:51:22 +0530

    Committing first upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost:27017/restful_blog_app",{useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false});
+
+//ROUTES
+const blogRoutes = require("./routes/blog");
+
+//MIDDLEWARE CONFIG
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+
+//ROUTING
+app.get("/", function(req, res){
+    res.redirect("/blog/blogs"); 
+ });
+app.use("/blog", blogRoutes);
+
+//PORT Config
+port = process.env.PORT || 1000;
+app.listen(port, () => {
+    console.log("SERVER IS RUNNING!");
+})
